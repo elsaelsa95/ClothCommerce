2f0d393cdecb4ea01f79d5796eff4af483c13806
Elsa Lustiana <elsa.lustiana@gmail.com>
2023-02-03 15:09:06 +0700

    fake db for product

--- clothes/pages/index.tsx
+++ clothes/pages/index.tsx
@@ -1,14 +1,39 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material'
 import { Inter } from '@next/font/google'
 import Layout from "./../components/Layout"
+import data from "../utils/data"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <Layout>
-    <div>
-      <h1>Products</h1>
-    </div>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.id}>
+              <Card sx={{ width: 300, height:400, justifyContent:"center" }}>
+                <CardActionArea>
+                  <CardMedia 
+                  component="img" 
+                  image={product.image} 
+                  title={product.name} 
+                  sx={{ width:300, height:300}}>
+                  </CardMedia>
+                  <CardContent>
+                    <Typography> {product.name} </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>$ {product.price}</Typography>
+                  <Button size="small" color="primary"> Add to Chart</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </Layout>
   )
 }
---
+++ clothes/utils/data.js
@@ -0,0 +1,82 @@
+const data = {
+  products: [
+    {
+      id: 1,
+      name: "Kulot Pants",
+      category: "Pants",
+      image:
+        "https://dynamic.zacdn.com/GbOMIs-2jxvUWeiMq2CL2SFRXiE=/fit-in/346x500/filters:quality(90):fill(ffffff)/https://static-id.zacdn.com/p/duapola-1992-3447723-1.jpg",
+      price: 7.16,
+      brand: "Atorvastatin",
+      rating: 5,
+      numReviews: 39,
+      countInStock: 9,
+      description: "A Popular Pants",
+    },
+    {
+      id: 2,
+      name: "Loose Pants",
+      category: "Pants",
+      image: "https://cdn.pomelofashion.com/img/p/5/2/7/0/4/6/527046.jpg",
+      price: 3.65,
+      brand: "Felodipine",
+      rating: 5,
+      numReviews: 70,
+      countInStock: 4,
+      description: "A Popular Pants",
+    },
+    {
+      id: 3,
+      name: "Satin Pajamas",
+      category: "Pajamas",
+      image:
+        "https://images.riverisland.com/is/image/RiverIsland/pink-satin-jacquard-pyjama-top_763460_main?$ProductListingPortrait$",
+      price: 2.15,
+      brand: "PM",
+      rating: 1,
+      numReviews: 99,
+      countInStock: 4,
+      description: "A Popular Pajamas",
+    },
+    {
+      id: 4,
+      name: "Polkadot Pajamas",
+      category: "Pajamas",
+      image:
+        "https://images.tokopedia.net/img/cache/500-square/product-1/2020/5/31/batch-upload/batch-upload_922ade77-281c-48c4-aaa9-92af28ff6ab3",
+      price: 2.15,
+      brand: "Acetaminophen",
+      rating: 1,
+      numReviews: 9,
+      countInStock: 4,
+      description: "A Popular Pajamas",
+    },
+    {
+      id: 5,
+      name: "Long Sleeve Blouse",
+      category: "Shirt",
+      image:
+        "https://ae01.alicdn.com/kf/Hf7d2b47f97654186a79c433f41e48323u/White-shirt-Women-s-New-Long-sleeve-Blouse-shirt-Loose-OL-Fashion-Button-Up-Satin-Silk.jpg",
+      price: 7.81,
+      brand: "Dextrose",
+      rating: 1,
+      numReviews: 85,
+      countInStock: 4,
+      description: "A Popular Shirt",
+    },
+    {
+      id: 6,
+      name: "Short Sleeve Blouse",
+      category: "Shirt",
+      image: "https://m.media-amazon.com/images/I/716vtPfdz0L._UX342_.jpg",
+      price: 9.9,
+      brand: "Oxygen",
+      rating: 3,
+      numReviews: 54,
+      countInStock: 1,
+      description: "A Popular Shirt",
+    },
+  ],
+};
+
+export default data;
